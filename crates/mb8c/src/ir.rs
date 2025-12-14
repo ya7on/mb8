94cd211bea03dc8ee67fa26c1daef982537249d4
Suprun <hello@syoma.website>
2025-12-14 22:25:06 +0400

    chore: init IR

--- crates/mb8c/src/ir.rs
+++ crates/mb8c/src/ir.rs
@@ -1,2 +1,32 @@
 #[derive(Debug)]
-pub enum IROpcode {}
+pub struct VirtualRegister(pub usize);
+
+#[derive(Debug)]
+pub struct BasicBlockId(pub usize);
+
+#[derive(Debug)]
+pub struct BasicBlock {
+    pub id: BasicBlockId,
+    pub next_id: BasicBlockId,
+    pub instructions: Vec<IRInstruction>,
+}
+
+#[derive(Debug)]
+pub enum BasicBlockTerminator {
+    Branch {
+        condition: VirtualRegister,
+        then_branch: BasicBlockId,
+        else_branch: BasicBlockId,
+    },
+    Jmp {
+        next: BasicBlockId,
+    },
+    Ret {
+        value: Option<VirtualRegister>,
+    },
+}
+
+#[derive(Debug)]
+pub enum IRInstruction {
+    Call,
+}
